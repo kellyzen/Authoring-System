844d8ae0e77abebc5e4391b817049cbd8296f90c
kellyzen <02tankelly@gmail.com>
2023-02-02 10:06:56 +0800

    Added add course popup

--- Dev/Dashboard/sidebar.php
+++ Dev/Dashboard/sidebar.php
@@ -1,19 +1,19 @@
 <div class="sidebar">
-    <div class="header-box">
-      <h3 class="header">Your course(s)</h1>
-    </div>
-
-    <ul class="course-lists list-unstyled">
-      <li class="course active"><a href="#" class="course-list text-decoration-none d-block"><span class="course-title"><i class="fal fa-regular fa-book"></i> Course title</span></a></li>
-      <li class="course"><a href="#" class="course-list text-decoration-none d-block"><span class="course-title"><i class="fal fa-regular fa-book"></i> Course title 11111111111111111111111111 000000000000</span></a></li>
-      <li class="course"><a href="#" class="course-list text-decoration-none d-block"><span class="course-title"><i class="fal fa-regular fa-book"></i> Course title 2</span></a> </li>
-      <li class="course"><a href="#" class="course-list text-decoration-none d-block"><span class="course-title"><i class="fal fa-regular fa-book"></i> Course title 3</span></a></li>
-      <li class="course"><a href="#" class="course-list text-decoration-none d-block"><span class="course-title"><i class="fal fa-regular fa-book"></i> Course title 4</span></a></li>
-    </ul>
-
-    <ul class="course-add list-unstyled">
-      <li class=""><a href="#" class="text-decoration-none d-flex justify-content-between">Add course<i class="fal fa-solid fa-plus"></i></a></li>
-
-    </ul>
-
+  <div class="header-box">
+    <span class="header">Your course(s)</span>
   </div>
+
+  <ul class="course-lists list-unstyled">
+    <li class="course active"><a href="#" class="course-list text-decoration-none d-block"><span class="course-title"><i class="fal fa-regular fa-book"></i> Course title</span></a></li>
+    <li class="course"><a href="#" class="course-list text-decoration-none d-block"><span class="course-title"><i class="fal fa-regular fa-book"></i> Course title 11111111111111111111111111 000000000000</span></a></li>
+    <li class="course"><a href="#" class="course-list text-decoration-none d-block"><span class="course-title"><i class="fal fa-regular fa-book"></i> Course title 2</span></a> </li>
+    <li class="course"><a href="#" class="course-list text-decoration-none d-block"><span class="course-title"><i class="fal fa-regular fa-book"></i> Course title 3</span></a></li>
+    <li class="course"><a href="#" class="course-list text-decoration-none d-block"><span class="course-title"><i class="fal fa-regular fa-book"></i> Course title 4</span></a></li>
+  </ul>
+
+  <ul id="course-add" class="course-add list-unstyled">
+    <li class=""><a class="text-decoration-none d-flex justify-content-between">Add course<i class="fal fa-solid fa-plus"></i></a></li>
+
+  </ul>
+
+</div>
