0abc4952c01647c5e371ac8369dec045835afbcc
kellyzen <02tankelly@gmail.com>
2023-03-13 23:48:21 +0800

    Added doc, vid, audio & image tab

--- Dev/Topic/file.php
+++ Dev/Topic/file.php
@@ -10,10 +10,10 @@ $count = mysqli_num_rows($query);
 
 if ($count != '0') { ?>
     <div class="tab-pane fade show active" id="nav-all" role="tabpanel" aria-labelledby="nav-all-tab"><?php include 'file_all.php'; ?></div>
-    <div class="tab-pane fade" id="nav-document" role="tabpanel" aria-labelledby="nav-document-tab">Document</div>
+    <div class="tab-pane fade" id="nav-document" role="tabpanel" aria-labelledby="nav-document-tab"><?php include 'file_document.php'; ?></div>
     <div class="tab-pane fade" id="nav-video" role="tabpanel" aria-labelledby="nav-video-tab"><?php include 'file_video.php'; ?></div>
-    <div class="tab-pane fade" id="nav-audio" role="tabpanel" aria-labelledby="nav-audio-tab">Audio</div>
-    <div class="tab-pane fade" id="nav-image" role="tabpanel" aria-labelledby="nav-image-tab">Image</div>
+    <div class="tab-pane fade" id="nav-audio" role="tabpanel" aria-labelledby="nav-audio-tab"><?php include 'file_audio.php'; ?></div>
+    <div class="tab-pane fade" id="nav-image" role="tabpanel" aria-labelledby="nav-image-tab"><?php include 'file_image.php'; ?></div>
     <div class="tab-pane fade" id="nav-text" role="tabpanel" aria-labelledby="nav-text-tab">Text</div> 
     <div class="tab-pane fade" id="nav-quiz" role="tabpanel" aria-labelledby="nav-quiz-tab">Quiz</div> 
 <?php } else {
