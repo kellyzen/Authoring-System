b3a78a10f75d0813780d2e6224cb7874d22b8876
ChanJinShuan <theonlychan1234@gmail.com>
2023-03-18 21:17:10 +0800

    Added topic invalid pages

--- Dev/Topic/topic_invalid.php
+++ Dev/Topic/topic_invalid.php
@@ -1,2 +1,4 @@
-<i class="fal fa-solid fa-ellipsis-h" ></i>
-<span>Topic invalid</span>
+<div class = "course-invalid-wrap">
+    <i class="fa fa-frown-o"></i>
+    <span>Sorry, no results were found for your query!</span>
+</div>
