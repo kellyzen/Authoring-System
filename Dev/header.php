334e7a6bea3c1e3a9c6e1c39c3e78290e551ce55
kellyzen <02tankelly@gmail.com>
2023-04-07 20:17:03 +0800

    added header logo

--- Dev/header.php
+++ Dev/header.php
@@ -1,5 +1,30 @@
-<!-- <div style="background: #464646; border: 1px solid; padding: 10px; box-shadow: 5px 10px #888888;" class="header" id="logo-container">
-  <a href="../Signup/signup.php">
-    <img style="height: 50px; display: block; margin-left: auto; margin-right: auto;" src="../../Image/logo.png" />
-  </a>
-</div> -->
+<style>
+  .logo-header {
+    width: 100%;
+    background: #464646;
+    box-shadow: 0px 5px 8px rgba(0, 0, 0, 0.25);
+    display: grid;
+    justify-content: center;
+    position: relative;
+    z-index: 10;
+  }
+
+  .logo {
+    font-weight: 600;
+    font-size: 26px;
+    color: #ccc;
+    padding-left: 30px;
+    text-decoration: none;
+    margin: 5px;
+  }
+
+  .logo:hover{
+    color: #ccc;
+    opacity: 0.7;
+    cursor: pointer;
+  }
+</style>
+
+<div class="logo-header">
+  <a class="logo">stud.io</a>
+</div>
